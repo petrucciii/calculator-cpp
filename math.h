99799a24515a1aedfa36edfc2a04deb718e37e84
rickypetrucci <75860927+petrucciii@users.noreply.github.com>
2021-08-20 19:58:49 +0200

    update-math

--- math.h
+++ math.h
@@ -1,1 +1,13 @@
+//by petrucciii
 
+#include "include/execute/add.hpp"
+#include "include/execute/subtract.hpp"
+#include "include/execute/multiply.hpp"
+#include "include/execute/divide.hpp"
+#include "include/execute/PI.hpp"
+
+#define EX_ADD doAdd()
+#define EX_SUBTRACT doSubtract()
+#define EX_MULTIPLY doMultiply()
+#define EX_DIVIDE doDivide()
+#define EX_PI doPI()
